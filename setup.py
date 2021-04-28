878836ff338880b9fa7e2516f1ae2f2e7e30d4c0
Cyker Way <cykerway@gmail.com>
2021-04-27 23:47:23 -0400

    -

--- setup.py
+++ setup.py
@@ -134,6 +134,8 @@ setup(
         ##  supported python versions;
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     ##  project keywords;
